a1f08ab812d6cfe3ee097bd107771e90d609d0a6
SuperCALIENTITO <70909800+SuperCALIENTITO@users.noreply.github.com>
2022-02-03 13:11:35 -0300

    Actualizaciones de alcalde

--- darkrp/addons/darkrpmodification/lua/autorun/debug.lua
+++ darkrp/addons/darkrpmodification/lua/autorun/debug.lua
@@ -1,3 +1,17 @@
--- Comandos de pruebas
+-- Funciones de prueba
 
--- print( team.GetName( Entity( 78 ):Team() ) )
+function IsPlayerTeam( ply, team )
+
+end
+
+-- Comandos de prueba
+
+if CLIENT and LocalPlayer():IsSuperAdmin() then
+
+	concommand.Add( "darkrp_setteam", function( ply, cmd, args)
+		local Team = args[1] or 1
+		ply:SetTeam( Team )
+		ply:Spawn()
+	end)
+
+end
