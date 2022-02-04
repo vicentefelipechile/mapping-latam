56c9d08d099311bb68dd2494745e408fc9e25907
SuperCALIENTITO <70909800+SuperCALIENTITO@users.noreply.github.com>
2022-02-04 00:33:57 -0300

    Añadido mas configuraciones

---
+++ darkrp/addons/z_easyskins/lua/z_easyskins/languages/cl_fr.lua
@@ -0,0 +1,140 @@
+-- [[ CREATED BY ZOMBIE EXTINGUISHER ]]
+
+local fr = {
+
+	-- general
+	yes = "Oui",
+	no = "Non",
+	close = "Fermer",
+	loading = "Chargement",
+	add = "Ajouter",
+	cancel = "Annuler",
+	choose = "Choisir",
+	clear = "Effacer",
+
+	menuTitle = "SKINS D'ARMES",
+	language = "Langue/Langage",
+
+	-- categories
+	settings = "Paramètres",
+	users = "Utilisateurs",
+	createSkins = "Créer des Skins",
+	manageSkins = "Gérer les Skins",
+	blacklist = "Liste noire",
+	shop = "Magasin",
+	inventory = "Inventaire",
+
+	-- settings
+	keybind = "Raccourcis clavier",
+	key = "Touche",
+	remoteShop = "Magasin distant", 
+	admin = "Administrateur",
+	donator = "Donateur",
+	donators = "Donateurs",
+	player = "Joueur",
+	players = "Joueurs",
+	shopPositions = "Emplacements des magasins",
+	addCurrPosition = "Ajouter l'emplacement actuel",
+	nameThePosition = "Nom de l'emplacement",
+	removePosition = "Vous allez supprimer l'emplacement <%s> !",
+	shopModel = "Modèle de magasin",
+	name = "Nom",
+	menuPrivileges = "Menu Privileges",
+	adminAccess = "Accès administrateur",
+	userAccess = "Accès utilisateur",
+	data = "Données",
+	resetDB = "Réinitialiser la base de données",
+	resetDBConfirmation = "Vous allez réinitialiser toutes les données sauvegardées!",
+	currencySymbol = "Symbole de la devise",
+	steamgroup = "Groupe Steam",
+	customULR = "URL personnalisée",
+	discount = "Réduction",
+	nameTag = "Nom d'étiquette",
+	tag = "Étiquette",
+	various = "Divers",
+	sellRate = "Taux de vente",
+	worldModelSkins = "Réseau de skin (Mondial)",
+	skinToWeaponName = "Ajouter le skin au nom de l'arme",
+	baseSkin = "Skin de base",
+
+	-- users
+	enterName = "Entrez votre nom",
+	loadPlayers = "Charger des joueurs", 
+	online = "En ligne",
+	offline = "Hors ligne",
+	removeSkinFromUser = "Vous allez supprimer le skin %s (%s) \nde %s",
+
+	-- admin give
+	giveSkin = "Donner un skin",
+	chooseSkin = "Chosir un skin",
+	skin = "Skin",
+	weapons = "Armes",
+	weaponsSelected = "Arme(s) séléctionée(s)",
+	chooseWeapons = "Choisir armes",
+	give = "Donner",
+
+	-- weapon list
+	category = "Catégorie",
+	class = "Classe",
+	name = "Nom",
+	saveSelection = "Sauvegarder la sélection",
+	viewmodel = "Viewmodel",
+	worldmodel = "Worldmodel",
+
+	-- create skins
+	step1 = "Étape 1 - Choisir le matériau",
+	chooseMaterial = "Choisir le matériau",
+	step2 = "Étape 2 - Armes utilisables",
+	applicableWeapons = "Armes utilisables",
+	step3 = "Étape 3 - Paramètres",
+	noMaterialSelected = "Aucun matériau sélectionné",
+	noWeaponsAdded = "Aucune arme ajoutée",
+	price = "Prix",
+	donatorPrice = "Prix du donateur",
+	donatorOnly = "Donateur seul",
+	createSkin = "Créer un skin",
+	enterNewCat = "Entrer une nouvelle catégorie",
+	catName = "Nom de la catégorie",
+	currency = "Devise",
+	default = "Défaut",
+	steamgroupOnly = "Groupe Steam uniquement",
+	nameTagOnly = "Nom d'étiquette uniquement",
+	purchasable = "Achetable",
+
+	-- material list
+	addSkin = "Ajouter un skin",
+	toggleMode = "Mode bascule",
+
+	-- skin edit
+	editSkin = "Éditer un skin",
+	material = "Matériau",
+	update = "Mise à jour",
+	weaponSelection = "Modifier la sélection d'arme (global)",
+
+	-- skin manage
+	removeCat = "Vous allez supprimer la catégorie <%s> !",
+	removeSkin = "Vous allez supprimer le skin <%s> !",
+
+	-- blacklist
+	selectWep = "Sélectionner une arme",
+
+	-- shop
+	buySkin = "Acheter <%s> pour <%s> ?",
+	emptyShop = "Aucun skin disponible à l'achat",
+	free = "Gratuit",
+
+	-- inventory
+	sellSkin = "Voulez vous vendre <%s> pour %s?",
+	emptyInventory = "Achetez votre premier skin au magasin",
+	giftSkin = "Voulez-vous donner <%s> à %s ?",
+	noPlayersFound = "Aucun joueur trouvé!",
+	sell = "Vente",
+	gift = "Don",
+	
+	-- notifications
+	notifications = "Notification de Skin reçu",
+	received = "Reçu"
+
+}
+
+return fr
---
+++ darkrp/addons/z_easyskins/lua/z_easyskins/languages/cl_ua.lua
@@ -0,0 +1,140 @@
+-- [[ CREATED BY ZOMBIE EXTINGUISHER ]]
+
+local ua = {
+	
+	-- general
+	yes = "Так",
+	no = "Ні",
+	close = "Закрити",
+	loading = "Завантаження",
+	add = "Додати",
+	cancel = "Скасувати",
+	choose = "Обрати",
+	clear = "Очистити",
+
+	menuTitle = "Знаряддя для шкіри",
+	language = "Мова",
+
+	-- categories
+	settings = "Налаштування",
+	users = "Користувачі",
+	createSkins = "Створити Шкіру",
+	manageSkins = "Управління шкірою",
+	blacklist = "Чорний список",
+	shop = "Магазин",
+	inventory = "Інвентар",
+
+	-- settings
+	keybind = "Ключові зв’язки",
+	key = "Ключ",
+	remoteShop = "Віддалений магазин", 
+	admin = "Адміністратор",
+	donator = "Донатор",
+	donators = "Донатори",
+	player = "Гравець",
+	players = "Гравці",
+	shopPositions = "Магазинний товар",
+	addCurrPosition = "Додати поточну позицію",
+	nameThePosition = "Назва позиції",
+	removePosition = "Ви збираєтесь видалити позицію <%s> !",
+	shopModel = "Модель магазину",
+	name = "Ім’я",
+	menuPrivileges = "Привілеї меню",
+	adminAccess = "Адміністративний доступ",
+	userAccess = "Користувацький доступ",
+	data = "Дані",
+	resetDB = "Перезавантажіті базу даних",
+	resetDBConfirmation = "Ви збираєтеся скинути всі збережені дані!",
+	currencySymbol = "Символ валюти",
+	steamgroup = "Steam Група",
+	customULR = "Спеціальна URL-адреса",
+	discount = "Знижка",
+	nameTag = "Тег імені",
+	tag = "Тег",
+	various = "Різноманітні",
+	sellRate = "Курс продажу",
+	worldModelSkins = "Мережеві шкіри (Worldmodel)",
+	skinToWeaponName = "Додати скін до імені зброї",
+	baseSkin = "Основа шкіри",
+
+	-- users
+	enterName = "Введіть ім’я",
+	loadPlayers = "Завантажити гравців", 
+	online = "Онайн",
+	offline = "Офлайн",
+	removeSkinFromUser = "Ви збираєтеся зняти шкіру %s (%s) \nfrom %s",
+
+	-- admin give
+	giveSkin = "Віддати шкіру",
+	chooseSkin = "Обрати шкіру",
+	skin = "Шкіра",
+	weapons = "Знаряддя",
+	weaponsSelected = "Знаряддя Обрано",
+	chooseWeapons = "Обрати знаряддя",
+	give = "Віддати",
+
+	-- weapon list
+	category = "Категорія",
+	class = "Клас",
+	name = "Ім’я",
+	saveSelection = "Зберегти вибір",
+	viewmodel = "Viewmodel",
+	worldmodel = "Worldmodel",
+
+	-- create skins
+	step1 = "Крок 1 - Виберіть Матеріал",
+	chooseMaterial = "Виберіть Матеріал",
+	step2 = "Крок 2 - Застосовувана зброя",
+	applicableWeapons = "Застосовувана зброя",
+	step3 = "Крок 3 - Налаштування",
+	noMaterialSelected = "не вибрано жодного матеріалу",
+	noWeaponsAdded = "не додана зброя",
+	price = "Ціна",
+	donatorPrice = "Ціна донора",
+	donatorOnly = "Тільки донатор",
+	createSkin = "Створити шкіру",
+	enterNewCat = "Введіть нову категорію",
+	catName = "Назва Категорії",
+	currency = "Валюта",
+	default = "За замовчуванням",
+	steamgroupOnly = "Тільки для Steam Група",
+	nameTagOnly = "Тільки тег імені",
+	purchasable = "Придбаний",
+
+	-- material list
+	addSkin = "Додати шкіру",
+	toggleMode = "Режим перемикання",
+
+	-- skin edit
+	editSkin = "Редагувати шкіру",
+	material = "Матеріал",
+	update = "Оновити",
+	weaponSelection = "Змінити вибір зброї (глобальний)",
+
+	-- skin manage
+	removeCat = "Ви збираєтесь видалити категорію <%s> !",
+	removeSkin = "Ви збираєтеся видалити шкіру <%s> !",
+
+	-- blacklist
+	selectWep = "Обрати зброю",
+
+	-- shop
+	buySkin = "Купити <%s> за <%s> ?",
+	emptyShop = "Жодної шкіри доступної для придбання",
+	free = "Безкоштовно",
+
+	-- inventory
+	sellSkin = "Ви хочете продати <%s> за %s?",
+	emptyInventory = "Придбайте Вашу першу шкіру в магазині",
+	giftSkin = "Ви хочете надати <% s> до % s?",
+	noPlayersFound = "Не знайдено гравців!",
+	sell = "Продати",
+	gift = "Подарунок",
+	
+	-- notifications
+	notifications = "Сповіщення про отримання шкіри",
+	received = "Отримано"
+	
+}
+
+return ua
