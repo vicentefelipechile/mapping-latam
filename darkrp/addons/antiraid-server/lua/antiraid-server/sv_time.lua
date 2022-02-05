a02e79474e205da59cd8b402e574b0ed9dd01b69
Lugent <35547583+Lugent@users.noreply.github.com>
2022-02-05 12:06:04 -0400

    Crear temporizador en sv_time.lua

--- darkrp/addons/antiraid-server/lua/antiraid-server/sv_time.lua
+++ darkrp/addons/antiraid-server/lua/antiraid-server/sv_time.lua
@@ -1,5 +1,6 @@
 -- local timing = os.date("%H%M%S", time)  <---  Esto incluia segundos
 
+--[[
 local time = os.time()
 local timing = os.date("%H%M", time)
 
@@ -19,4 +20,14 @@ elseif timing <= time_end then
 
 else
 	print("Wotefok?!")
+end]]--
+
+local time_cooldown = (24 * (60 * 60)) -- Each 24 hours
+local function MP_RunTimer()
+	local system_time = os.time() -- Current system's time
+	local system_Time_next = cookie.GetNumber("MPCookie_ServerTime", 0) -- Saved next system's time for checking
+	if (system_time >= system_time_next) then -- The cooldown ended?
+		cookie.Set("MPCookie_ServerTime", system_time + time_cooldown) -- Okay, reset the cooldown back to 24 hours
+	end
 end
+hook.Add("Tick", "MP_AutoTimer", MP_RunTimer) -- Hook to keep it running
