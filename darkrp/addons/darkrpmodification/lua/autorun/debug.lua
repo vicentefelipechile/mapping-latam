744e708c33451d94fb1e8c5b78fa4bb9ba6c65d9
SuperCALIENTITO <70909800+SuperCALIENTITO@users.noreply.github.com>
2022-02-07 10:12:31 -0300

    Update debug.lua

--- darkrp/addons/darkrpmodification/lua/autorun/debug.lua
+++ darkrp/addons/darkrpmodification/lua/autorun/debug.lua
@@ -1,17 +1,32 @@
--- Funciones de prueba
+-- Funciones
+local function AutoComplete( cmd, args )
+	print( cmd, args)
 
-function IsPlayerTeam( ply, team )
+	args = string.Trim( args )
+	args = string.lower( args )
 
-end
+	local table1 = {}
+
+	for k, v in ipairs(player.GetAll()) do
+		local nick = v:Nick()
+		if string.find( string.lower( nick ), args) then
+			nick = "\"" .. nick "\""
+			nick = "darkrp_setteam " .. nick
+
+			table.insert(table1, nick)
+		end
+	end
 
 -- Comandos de prueba
-
-if CLIENT and LocalPlayer():IsSuperAdmin() then
+if CLIENT then
 
 	concommand.Add( "darkrp_setteam", function( ply, cmd, args)
-		local Team = args[1] or 1
-		ply:SetTeam( Team )
-		ply:Spawn()
-	end)
+		if not ply:IsSuperAdmin() then return end
+		local target = args[0]
+		local Team = args[1]
+		target:SetTeam( Team )
+		target:Spawn()
+	end,
+	AutoComplete)
 
 end
