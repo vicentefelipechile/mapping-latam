fd2be46157929f4066ed38f856005a3acaf91f88
SuperCALIENTITO <70909800+SuperCALIENTITO@users.noreply.github.com>
2022-02-11 15:50:49 -0300

    Update disable-pickupcamera.lua

--- darkrp/addons/darkrpmodification/lua/autorun/disable-pickupcamera.lua
+++ darkrp/addons/darkrpmodification/lua/autorun/disable-pickupcamera.lua
@@ -1,5 +1,7 @@
 hook.Add( "PhysgunPickup", "AllowPlayerPickup", function( ply, ent )
-	if ( ply:IsSuperAdmin() and ent:GetClass() == "gmod_cameraprop" ) then
+	if ply:IsSuperAdmin() and ent:GetClass() == "gmod_cameraprop" then
 		return true
+	else
+		return false
 	end
 end )
