c02956b33ef9a8212ba5c438ff973165f24b716c
SuperCALIENTITO <70909800+SuperCALIENTITO@users.noreply.github.com>
2022-02-12 11:11:54 -0300

    Añadido traducción de MRS

---
+++ darkrp/addons/msd_ui/lua/msd/language/es.lua
@@ -0,0 +1,255 @@
+﻿MSD.Language["es"] = {
+
+	-- UI
+
+	lang_name = "Spanish",
+
+	ok = "OK",
+	map = "Mapa",
+	off = "Apagado",
+	on = "Encendido",
+	time_add = "Tiempo para agregar",
+	type = "Tipo",
+	delay = "Retraso",
+	cancel = "Cancelar",
+	enable = "Permitir",
+	model = "Modelo",
+	name = "Nombre",
+	settings = "Ajustes",
+	editor = "Editor",
+	red = "Rojo",
+	green = "Verde",
+	blue = "Azul",
+	admin_menu = "Menú de admin",
+	ui_settings = "Ajustes de interfaz",
+	active = "Activo",
+	inactive = "Inactivo",
+	disabled = "Deshabilitado",
+	warning = "Peligro",
+	remove = "Eliminar",
+	theme = "Tema",
+	dark_theme = "Tema oscuro",
+	payment = "Pago",
+	load_autosave = "Cargar el ultimo autoguardado?",
+	load_save = "Cargar guardado",
+	create_new = "Crear nuevo",
+	enable_option = "Permitir opción",
+	main_opt = "Opciones principales",
+	copy_data = "Copiar datos",
+	save_chng = "Guardar cambios",
+	enter_name = "Ingresar el nombre",
+	enter_id = "Ingresar el ID",
+	confirm_action = "Por favor confirma tus acciones",
+	check_fpr_errors = "Chequear errores",
+	enter_description = "Ingresar descripción",
+	cooldown_ok = "Enfriamiento en el éxito",
+	cooldown_fail = "Enfriamiento en caso de falla",
+	s_team_whitelist = "Configurar la lista blanca del equipo",
+	whitelist_blacklist = "La lista blanca es una lista negra",
+	custom_val = "Colocar valor personalizado",
+	set_hp_full = "Colocar todo el HP",
+	dist_to_close = "Distancia al más cercano",
+
+	e_text = "Ingresar texto",
+	e_number = "Ingresar número",
+	e_class = "Ingresar clase",
+	e_value = "Ingresar valor",
+	e_blank_dis = "Dejar en blanco para deshabilitar",
+	e_blank_default = "Dejar en blanco para usar por defecto",
+	e_url = "Ingresar URL",
+	e_model = "Ingresar ruta del modelo",
+	e_material = "Ingresar ruta del material",
+	e_wep_class = "Ingresar ruta de la arma",
+	e_ent_class = "Ingresar ruta de la entidad",
+	e_veh_class = "Ingresar ruta del vehículo",
+	e_npc_class = "Ingresar clase del NPC",
+
+	select_ammo = "Munición seleccionada",
+	amount_ammo = " Cantidad munición",
+	disable_phys = "Deshabilitar la física",
+	none = "Ninguno",
+	custom_icon = "Seleccionar icono personalizado",
+	weapon_name = "Nombre del arma",
+	moveup = "Mover hacia arriba",
+	movedown = "Mover hacia abajo",
+	movepoint = "Mover punto",
+	swap = "Intercambiar",
+	swapmod = "Intercambiar mod habilitado. Click para deshabilitar",
+	copy_from_ent = "Copiar de la entidad que mira",
+	set_pos_self = "Establecer en su posición",
+	set_pos_aim = "Establecer en el punto de mira",
+	spawn_point = "Spawnear punto",
+	spawn_ang = "Spawnear ángulo",
+	mark_area = "Marcar área",
+	time_wait = "Tiempo de espera",
+	map_marker = "Seleccionar punto del mapa",
+	in_sec = "En segundos",
+	def_units = "Default %s unidades", -- "350 unidades por defecto" leave %s as is
+	def_seconds = "Default %s segundos", -- "10 segundos por defecto" leave %s as is
+	ent_show_pointer = "Mostrar puntero sobre la entidad",
+	ent_arcade_style = "Apariencia de entidad de estilo arcade",
+	ent_stnd_style = "Apariencia de la entidad estándar",
+	custom_color = "Habilitar color personalizado",
+	mat_default = "Dejar en blanco para material por defecto",
+
+	set_ui = "Ajustes de la interfaz de usuario",
+	set_hud = "Ajustes del HUD",
+	set_hud_pos = "Posición HUD de las misiones",
+	set_hud_themes = "Temas del HUD",
+	set_server = "Ajustes de servidor",
+	set_ui_blur = "Desenfoque del fondo",
+	set_ui_mono = "Fondo monocromático",
+	set_ui_vignette = "Efecto de viñeta para el fondo",
+	set_ui_brightness = "Brillo de fondo",
+	set_ui_color = "Seleccionar el color principal",
+	set_ui_align_left = "Alineación vertical hacia la izquierda",
+	set_ui_align_right = "Alineación vertical hacia la derecha",
+	set_ui_align_top = "Alineación vertical hacia arriba",
+	set_ui_align_bottom = "Alineación vertical hasta la parte inferior",
+	set_ui_offset_h = "Compensación horizontal",
+	set_ui_offset_v = "Compensación vertical",
+
+	upl_changes = "Subir cambios al servidor",
+	res_changes = "Restablecer cambios",
+
+	-- Jugador
+
+	dead = "Estas muerto",
+	time_ex = "Tiempo expirado",
+	vehicle_bum = "Tu Vehículo esta destruido",
+	left_area = "Dejaste la zona",
+	m_blew = "You blew up the mission",
+	m_failed = "Misión fallida",
+	m_success = "Misión exitosa",
+	m_loop = "Actualización de la misión",
+
+	-- Errores
+
+	inv_quest = "Misión Invalida",
+	team_bl = "Tu equipo está en lista negra",
+	no_players = "El servidor necesita más jugadores conectados antes que puedas hacer esto",
+	no_players_team = "El servidor necesita más jugadores conectados para equipo(s) especifico(s)antes que puedas hacer esto",
+	need_admin = "Solo los administradores pueden realizar esta accion",
+
+	-- Misiones
+
+	active_quest = "Tienes una misión activa",
+	inactive_quest = "No puedes jugar esta misión",
+	quest_editor = "Editor de misiones",
+	quest_list = "Lista de misiones",
+	quests = "Misiones",
+	leave_pnt = "Dejar punto",
+
+	q_editobj = "Editar objetivos",
+	q_incvobj = "Objetivo invalido",
+	q_setobj = "Ajustes de objetivo",
+	q_newobj = "Añadir nuevo objetivo",
+	q_editrwd = "Editar recompensas",
+	q_rwdeditor = "Editor de recompensas",
+	q_rwdlist = "Lista de recompensas",
+	q_rwdsets = "Ajustes de recompensas",
+	q_findmap = "Encontrar misiones de otros mapas",
+	q_obj_des = "Descripción de objetivo",
+	q_dist_point = "Distancia al punto",
+	q_dist_from_point = "Distancia desde el punto",
+	q_ignore_veh = "Ignorar vehículo de misión",
+	q_timer_show = "Mostrar el temporizador al jugador",
+	q_area_stay = "El jugador debe quedarse en el area",
+	q_start = "Empezar Misión",
+	q_new = "Nueva misión",
+	q_submit = "Enviar misión",
+	q_addnew = "Añadir nueva misión",
+	q_remove = "Eliminar misión",
+	q_id_unique = "La ID debe ser única para cada misión",
+	q_complete_msg = "Mensaje de misión completada",
+	q_dotime = "Tiempo para hacer la misión",
+	q_dotime_ok = "Misión exitosa al finalizar el tiempo",
+	q_dotime_fail = "Falla la misión al finalizar el tiempo",
+	q_death_fail = "Falló la misión al morir el jugador",
+	q_loop = "Tareas de misiones en bucle",
+	q_loop_reward = "Recompensa al jugador en cada bucle",
+	q_enable = "Habilitar misión",
+	q_events = "Eventos",
+	q_eventadd = "Añadir Evento",
+	q_eventedit = "Editor de evento",
+	q_eventremove = "Eliminar evento",
+	q_in_progress = "Misión en progreso",
+	q_time_left = "Tiempo restante",
+	q_ply_limit = "Limite de jugadores para la misión",
+	q_ply_team_limit = "Configurar equipos limitados",
+	q_ply_team_need = "Jugadores de equipo necesarios",
+	q_ply_need = "Cantidad de jugadores necesarios para comenzar",
+	q_play_limit = "Hay un límite en cuantos jugadores pueden jugar esta misión",
+	q_must_stay_area = "Debes permanecer dentro de esta área, o la misión fallara",
+	q_time_wait = "Debes esperar antes de repetir esta misión",
+	q_dotime_reset = "Restablecer el tiempo de realización de la misión",
+	q_dotime_add = "Añadir tiempo para hacer la misión",
+	q_noreplay = "No puedes re jugar esta misión",
+	q_dis_replay = "Des habilitar repetición de la misión",
+	q_needquest = "Debes completar otra misión primero",
+	q_needquest_menu = "Requiere misión completada",
+	q_enterror = "Entidades de misión no spawnean, chequear configuración de misión",
+	q_get = "Puedes obtener una misión de estos NPCS",
+	q_noquests = "Todavía no hay forma de jugar las misiones :(",
+	q_ent_draw = "Distancia del dibujo de la entidad",
+	q_loop_stop_key = "Tecla para detener misión en bucle",
+	q_hold_key_stop = "Para detener la misión, mantenga [%s]", -- Para detener la mision mantenga apretado [P]
+	q_enter_veh = "Entrar a tu vehículo",
+	q_npc_link = "Linquear misión a un NPC",
+	q_icon68 = "Ingresar url a un icono .PNG de 68x68 px",
+	q_ent_pos_show = "Mostrar locación de entidades al jugador",
+	q_area_size = "Tamaño de área",
+	q_area_pos = "Posición de área",
+	q_s_area_size = "Buscar tamaño de área",
+	q_s_area_pos = "Buscar posición de área",
+	q_npc_answer_ok = "Respuesta positiva del jugador",
+	q_npc_answer_no = "Respuesta negativa del jugador",
+	q_npc_answer_noq = "Respuesta del jugador si no hay misiones",
+	q_npc_quest_no = "Discurso del NPC si no hay misiones",
+	q_money_give = "Dinero para dar",
+
+	-- Simple NPCs
+
+	npc_editor = "Editor de NPC",
+	npc_new = "Nuevo NPC",
+	npc_select = "Seleccionar un NPC",
+	npc_e_speech = " Ingresar el discurso del NPC",
+	npc_submit = "Confirmar creación del NPC",
+	npc_update = "Actualizar NPC",
+	npc_remove = "Eliminar NPC",
+	npc_q_enable = "Habilitar misiones de NPCs",
+	npc_did_open = "ID de dialogo para abrir",
+	npc_q_target = "NPC is an objective target",
+	npc_hostile = "NPC Hostil",
+
+}
+
+-- Otras frases
+local es = "es"
+
+MSD.Language[es]["Move to point"] = "Mover al punto"
+MSD.Language[es]["Leave area"] = "Dejar área"
+MSD.Language[es]["Kill NPC"] = "Matar NPC"
+MSD.Language[es]["Collect quest ents"] = "Recoger misiones"
+MSD.Language[es]["Talk to NPC"] = "Hablar al NPC"
+
+MSD.Language[es]["No hay misiones disponibles"] = "No hay misiones disponibles"
+
+MSD.Language[es]["Give weapon"] = "Dar arma"
+MSD.Language[es]["Give ammo"] = "Dar munición"
+MSD.Language[es]["Strip Weapon"] = "Extraer arma"
+MSD.Language[es]["Spawn quest entity"] = "Spawnear entidad de misión"
+MSD.Language[es]["Spawn entity"] = "Spawnear entidad"
+MSD.Language[es]["Spawn npc"] = "Spawnear npc"
+MSD.Language[es]["Manage do time"] = "Administrar el tiempo"
+MSD.Language[es]["Spawn vehicle"] = "Spawnear vehículo"
+MSD.Language[es]["Remove vehicle"] = "Elminar vehículo"
+MSD.Language[es]["Remove all entites"] = "Remover todas las entidades"
+MSD.Language[es]["Set HP"] = "Configurar HP"
+MSD.Language[es]["Set Armor"] = "Configurar armadura"
+
+MSD.Language[es]["DarkRP Money"] = " Dinero DarkRP"
+
+MSD.Language[es]["Quest NPCs are disabled"] = "Los NPCs de las misiones están deshabilitados"
+MSD.Language[es]["You can enable them in settings"] = "Puedes habilitarlos en las configuraciones"
+MSD.Language[es]["Wait time"] = "Tiempo de espera"
