11f3ab2b289e32674c7a8b07d3d00c830801695e
Lugent <35547583+Lugent@users.noreply.github.com>
2022-02-17 22:52:50 -0400

    Los paréntesis. AAAAAAAAAAAA

--- darkrp/addons/darkrpmodification/lua/autorun/alcalde.lua
+++ darkrp/addons/darkrpmodification/lua/autorun/alcalde.lua
@@ -1,5 +1,5 @@
-hook.Add( "player_spawn", "SetHealthMayor", function( ply )
-	if ply:Team() == TEAM_MAYOR then
+hook.Add("player_spawn", "SetHealthMayor", function(ply)
+	if (ply:Team() == TEAM_MAYOR) then
 		ply:SetHealth(200)
 		ply:SetMaxHealth(200)
 		ply:SetArmor(200)
