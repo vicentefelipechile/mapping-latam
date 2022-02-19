f24c3a84ba1202a38addb4dedc85e15f9fdde19e
SuperCALIENTITO <70909800+SuperCALIENTITO@users.noreply.github.com>
2022-02-18 23:32:28 -0300

    Mediaplayer

--- darkrp/addons/darkrpmodification/lua/entities/mediaplayer_tv_darkrp/shared.lua
+++ darkrp/addons/darkrpmodification/lua/entities/mediaplayer_tv_darkrp/shared.lua
@@ -1,29 +1,18 @@
-AddCSLuaFile()
-
-DEFINE_BASECLASS( "mediaplayer_base" )
-
-ENT.PrintName 		= "Big Screen TV"
-ENT.Author 			= "Samuel Maddock"
-ENT.Instructions 	= "Right click on the TV to see available Media Player options. Alternatively, press E on the TV to turn it on."
-ENT.Category 		= "Media Player"
-
 ENT.Type = "anim"
-ENT.Base = "mediaplayer_base"
+ENT.Base = "base_gmodentity"
 
+ENT.PrintName = "Generator"
 ENT.Spawnable = true
+ENT.Category = "Bitminers 2"
 
-ENT.Model = Model( "models/gmod_tower/suitetv_large.mdl" )
-
-list.Set( "MediaPlayerModelConfigs", ENT.Model, {
-	angle = Angle(-90, 90, 0),
-	offset = Vector(6, 59.49, 103.65),
-	width = 119,
-	height = 69
-} )
+ENT.RenderGroup = RENDERGROUP_TRANSLUCENT
 
 function ENT:SetupDataTables()
-	BaseClass.SetupDataTables( self )
+	self:NetworkVar( "Bool", 1, "IsOn" )
+	self:NetworkVar( "Bool", 2, "ShowToMuchPowerWarning")
+	self:NetworkVar( "Bool", 3, "ShowNoFuelWarning")
+	self:NetworkVar( "Int", 1, "FuelLevel")
+	self:NetworkVar( "Float", 2, "PowerConsumpsion")
 
-	self:NetworkVar( "String", 1, "MediaThumbnail" )
 	self:NetworkVar( "Entity", 0, "owning_ent" )
 end
